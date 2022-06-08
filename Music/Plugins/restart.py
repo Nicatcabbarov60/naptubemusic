5c860cc2821ae4a52a66224d2234f40193e94578
Cabbarov777 <103649914+Cabbarov777@users.noreply.github.com>
2022-06-08 08:30:50 +0400

    Update restart.py

--- Music/Plugins/restart.py
+++ Music/Plugins/restart.py
@@ -27,7 +27,7 @@ async def theme_func(_, message):
         try:
             await app.send_message(
                 x,
-                "Music has just restarted herself. Sorry for the issues.\n\nStart playing after 10-15 seconds again.",
+                "15-20 Saniyə sora /start yazın 💁.",
             )
         except Exception:
             pass
